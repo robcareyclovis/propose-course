17908431da6452d35c2a34bd7197989beb9f8be8
robcareyclovis <robcarey65@gmail.com>
2026-01-28 12:10:59 -0800

    Storyline Course files

---
+++ story_content/6ScSZy854yG_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6ScSZy854yG_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.108.36078.0%0D%0A%0D%0A00:00:00.100%20--%3E%2000:00:09.957%0D%0A[Jazzy%20blues%20you%20would%20hear%20in%20winter]%0D%0A%0D%0A"}]})
